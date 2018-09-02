f24744a99377f980fa31135807e35fb3d9b2d56d
Elena <elena.semenova_2012@mail.ru>
2018-09-02 12:23:24 +0300

    Module8.Lesson3.Part2

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-var myNumber = 24;
-console.log(myNumber);
+var a = 5;
+var b = -7;
+var d = 2;
+var c =  (a + b) * d;
+console.log(c);
 
-var age = 17;
-console.log(age);
-console.log(typeof age);
+console.log(a==b);
+var e = 5;
+
+console.log(a==e);
+
+var f = "5";
+
+console.log(a==f);
+console.log(a===f);
+
